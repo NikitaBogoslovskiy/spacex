950cb687d156c4d055166b29b2c3b314e135b906
NikitaBogoslovskiy <nbog@sfedu.ru>
2023-10-29 20:25:30 +0300

    do lab

--- src/api/spacex.js
+++ src/api/spacex.js
@@ -7,14 +7,37 @@ class SpaceX{
         return fetch(`${this.baseUrl}launches`)
             .then(response=>response.json())
     }
-    launchpads(){
+    launchpads(geoJSON = false){
         return fetch(`${this.baseUrl}launchpads`)
             .then(response=>response.json())
+            .then(json => geoJSON ? this.#convertToGeoJSON(json) : json);
     }
     launchpad(id){
         return fetch(`${this.baseUrl}launchpads/${id}`)
             .then(response=>response.json())
-
+    }
+    #convertToGeoJSON(input_json){
+        let geoJSON = {
+            "type":"FeatureCollection",
+            "features":[] 
+        };
+        for(let i = 0; i < input_json.length; i++){
+            geoJSON.features.push({
+                "type":"Feature",
+                "properties":{
+                    "id":input_json[i].id,
+                    "name":input_json[i].name
+                },
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[
+                        input_json[i].longitude,
+                        input_json[i].latitude
+                    ]
+                }
+            });
+        }
+        return geoJSON;
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -6,23 +6,45 @@ document.addEventListener("DOMContentLoaded", setup)
 
 function setup(){
     const spaceX = new SpaceX();
-    spaceX.launches().then(data=>{
-        const listContainer = document.getElementById("listContainer")
-        renderLaunches(data, listContainer);
-        drawMap();
-    })
+    spaceX.launches().then(launches=>{
+        spaceX.launchpads(true).then(launchPads=>{
+            const listContainer = document.getElementById("listContainer")
+            renderLaunches(launches, listContainer);
+            drawMap(launchPads);
+        });
+    });
 }
+
 function renderLaunches(launches, container){
     const list = document.createElement("ul");
     launches.forEach(launch=>{
         const item = document.createElement("li");
         item.innerHTML = launch.name;
+        item.addEventListener("click", changeColor, false);
+        item.launchPadId = launch.launchpad
         list.appendChild(item);
     })
     container.replaceChildren(list);
 }
 
-function drawMap(){
+function changeColor(event){
+    const launchPads = d3.select('#map')
+        .select("svg")
+        .select("g[name='launchPads']");
+    const selectedPadCoords = launchPads.select(`path[id='${event.currentTarget.launchPadId}']`).data()[0].geometry.coordinates;
+    launchPads.selectAll("path")
+        .attr("class", function(lp) {
+            if (lp.properties.id == event.currentTarget.launchPadId)
+                return "selectedLaunchPad";
+            const sqrDistance = (lp.geometry.coordinates[0] - selectedPadCoords[0]) * (lp.geometry.coordinates[0] - selectedPadCoords[0])
+             + (lp.geometry.coordinates[1] - selectedPadCoords[1]) * (lp.geometry.coordinates[1] - selectedPadCoords[1]);
+            if (sqrDistance < 0.1)
+                return "transparentLaunchPads";
+            return "launchPads";
+        });
+}
+
+function drawMap(launchPads){
     const width = 640;
     const height = 480;
     const margin = {top: 20, right: 10, bottom: 40, left: 100};
@@ -37,6 +59,7 @@ function drawMap(){
         .center([0,20])
         .translate([width / 2 - margin.left, height / 2]);
     svg.append("g")
+        .attr("name", "map")
         .selectAll("path")
         .data(Geo.features)
         .enter()
@@ -46,7 +69,20 @@ function drawMap(){
             .projection(projection)
         )
         .attr("fill", function (d) {
-            return colorScale(0);
+            return 0;
         })
-        .style("opacity", .7)
+        .style("opacity", 1);
+    svg.append("g")
+        .attr("name", "launchPads")
+        .selectAll('.launchPads')
+		.data(launchPads.features)
+		.enter()
+		.append('path')
+        .attr('id', function(lp){
+            return lp.properties.id;
+        })
+		.attr('d', d3.geoPath()
+            .projection(projection)
+            .pointRadius(5))
+		.attr('class', 'launchPads');
 }
